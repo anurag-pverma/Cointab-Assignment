332bb3707113a2f3b6b5613895b79cde14e1c8e6
Anurag Verma <anurag.premaverma@gmail.com>
2022-12-07 11:36:43 +0530

    app is created

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div>
+      <div>
+      <form >
+
+        
+
+
+      </form>
+    
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
